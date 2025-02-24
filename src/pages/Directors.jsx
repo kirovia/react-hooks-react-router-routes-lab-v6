f7ca5290337ad573962b7c2a80a67c0e9f0654ba
Joseph Andrew Phillips <josephandrewphillips@gmail.com>
2025-02-24 16:31:23 -0500

    Passes all tests

--- src/pages/Directors.jsx
+++ src/pages/Directors.jsx
@@ -1,13 +1,35 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
 
 function Directors() {
+
+  const [directors, setDirectors] = useState([])
+
+  useEffect(() =>{
+    fetch('http://localhost:4000/directors')
+      .then(resp => resp.json())
+      .then(data => setDirectors(data))
+  }, [])
+
+  const directorsToDisplay = directors.map(director => {
+    return (
+      <article key={director.id}>
+        <h2>{director.name}</h2>
+        <ul>
+          {director.movies.map(movie => <li key={movie}>{movie}</li>)}
+        </ul>
+      </article>
+    )
+  })
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Director info here! */}
+        <h1>Directors Page</h1>
+        {directorsToDisplay}
       </main>
     </>
   );
--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,13 +1,33 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import NavBar from "../components/NavBar";
 
 function Movie() {
+
+  const params = useParams();
+  const [movieTitle, setMovieTitle] = useState('')
+  const [movieGenres, setMovieGenres] = useState([])
+  const [movieTime, setMovieTime] = useState(0);
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/movies/${params.id}`)
+      .then(resp => resp.json())
+      .then(data => {
+        setMovieTitle(data.title)
+        setMovieGenres(data.genres)
+        setMovieTime(data.time)
+      })
+  }, [])
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Movie info here! */}
+        <h1>{movieTitle}</h1>
+        <p>{movieTime} minutes</p>
+        {movieGenres.map(genre => <span key={genre}>{genre}</span>)}
       </main>
     </>
   );
